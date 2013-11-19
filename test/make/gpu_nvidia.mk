984a65038cfb8097b316d19f37d1821a1b26d9f9
tlepley <ikolomus@yahoo.com>
2013-11-19 18:46:50 +0100

    Now tests work on Nvidia GPU

--- test/make/gpu_nvidia.mk
+++ test/make/gpu_nvidia.mk
@@ -1,14 +1,14 @@
 ifndef OCL_INSTALL_DIR
-OCL_INSTALL_DIR = /usr
+OCL_INSTALL_DIR = /usr/local/cuda
 endif
 
 # Host compilation
 HOST_CC = gcc
-OPENCL_CFLAGS += -I$(OCL_INSTALL_DIR)/include/nvidia-current
-OPENCL_LDFLAGS += -L$(OCL_INSTALL_DIR)/lib -lOpenCL
+OPENCL_CFLAGS += -I$(OCL_INSTALL_DIR)/include
+OPENCL_LDFLAGS += -L$(OCL_INSTALL_DIR)/lib64 -lOpenCL
 
 # Execution
-RUN_ARGS += -vendor Nvidia
+RUN_ARGS += -vendor NVIDIA
 
 # Build directory
 PLT_BUILD_DIR = $(BUILD_DIR)_gpu_nvidia
